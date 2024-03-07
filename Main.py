d2a6df14d4bb293d05410ab87226fefd72d21de3
luci4_4 <francevdaniil9@gmail.com>
2024-03-07 20:26:49 +1000

    Test commit

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Hello, this is Esyatext, text redactor<-->")
+print("Hello, this is Esyatext, text redactor<-->\n")
+
+# Text recognition and output
+
+import pytesseract
+from PIL import Image
+
+# Download image
+image = Image.open("IMG/screen3.jpg")
+
+image = image.convert("L")
+image = image.point(lambda x: 0 if x <= 128 else 255)
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\franc\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+
+# Text recognition
+
+text = pytesseract.image_to_string(image, lang='rus')
+
+print(f"Result: \n{text}")
