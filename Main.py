fe7906074512ed4695661961addec4053fa70d6b
opc0der <39023500+opc0der@users.noreply.github.com>
2024-04-16 22:23:40 +1000

    main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,32 @@
-print("Hello, this is Esyatext, text redactor<-->")
+import telebot
+import pytesseract
+from PIL import Image
+
+API_TOKEN = '6829766269:AAED7DVmgkwNZnUQmBdo6AIqiyhGnkeaWPU'
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}, я перепишу с фотографии любой печатный текст, который ты мне отправишь')
+
+
+@bot.message_handler(commands=['photo'])
+def main(message):
+    bot.send_message(message.chat.id,'Пожалуйста, отправьте фотографию')
+
+@bot.message_handler(content_types=['photo'])
+def handle_image(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    file = bot.download_file(file_info.file_path)
+    with open('image.jpg', 'wb') as f:
+        f.write(file)
+
+    image = Image.open('image.jpg')
+    text = pytesseract.image_to_string(image, lang='eng+rus')
+
+    response = f'{text}'
+    bot.reply_to(message, response)
+
+
+bot.polling()
